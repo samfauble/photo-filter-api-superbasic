25bbf0ef22473178cca21a797090ce6c7196346f
samfauble <samfauble@gmail.com>
2020-05-01 00:59:06 -0300

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -14,7 +14,7 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
 
-  app.get("/filteredimage", async (req, res) => {
+  app.get("/filteredimage", async (req: Request, res: Response) => {
     
     const url = req.query.image_url
     
